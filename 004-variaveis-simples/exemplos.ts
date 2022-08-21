bb165310684bdb5ca822a05329bab03d24495020
Lucas Floriani <lucas.floriane@gmail.com>
2022-08-20 23:11:36 -0300

    feat: add examples of simple variables

--- 004-variaveis-simples/exemplos.ts
+++ 004-variaveis-simples/exemplos.ts
@@ -1,19 +1,19 @@
-// ┌─────────────────┐
-// │                 │
-// │      Caixa      │
-// │        de       │
-// │     Variável    │
-// │                 │
-// │                 │
-// └─────────────────┘
+// === Exemplos de variáveis String ===
+var nome = "Lucas Floriani";
+var rua = "Avenida Cruzeiro do Sul";
+var descricao = "Sou uma descrição de uma lava roupa com RGB da Razer!";
 
-// ==== Tipos de Dados Primitivo ====
-var nome = "Lucas Floriani"; // String  => Texto
-var idade = 26; // Integer => Número Inteiro
-var dinheiro = 2.0; // Float   => Número Decimal
-var maiorDeIdade = true; // Boolean => Sim ou Não (true ou false)
+// === Exemplos de variáveis Integer ===
+var idade = 26;
+var quantidadeDeVogais = 5;
+var bananasEmUmaPenca = 14;
 
-// ==== Formas de escrever o nome da variável ====
-// camelCase => primeiroNome
-// PascalCase => PrimeiroNome
-// snake_case => primeiro_nome
+// === Exemplos de variáveis Float ===
+var precoDoIogurte = 2.08;
+var temperatura = -1.3;
+var distanciaCidadeVizinha = 15.6;
+
+// === Exemplos de variáveis Boolean ===
+var maiorDeIdade = true;
+var compreiFifa = false;
+var souProgramador = true;
